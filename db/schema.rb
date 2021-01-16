3945cbf9beb9c8ac755463ea5555f90e265e5611
xpointersite@outlook.com <xpointersite@outlook.com>
2021-01-15 21:09:06 -0300

    fix for migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_01_14_015101) do
 
   create_table "users", force: :cascade do |t|
-    t.string "user"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
